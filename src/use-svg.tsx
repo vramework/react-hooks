eccb155f025c8eb9277f888dad16de8605c3e0f8
Yasser Fadl <yasser.fadl@gmail.com>
2021-11-07 09:18:33 +0000

    fix: default icon for when svg is loading

--- src/use-svg.tsx
+++ src/use-svg.tsx
@@ -1,31 +1,22 @@
 import React from 'react'
 import SVG from 'react-inlinesvg'
 
-type DefaultIconKeys = 'info' | 'user' | 'generic'
-const defaultIcons: Record<DefaultIconKeys, React.ReactElement | null> = {
-    info:  null,
-    user: null,
-    generic: null
-}
-
 export const useSVG = (svgName: string, className: string = 'w-4 h-4', color: string = 'currentColor') => {
-    const defaultIcon: DefaultIconKeys = 'generic'
     return <SVG
         className={className}
         src={`/svg/${svgName}`}
         cacheRequests={true}
-        loader={defaultIcons[defaultIcon] || null}
+        loader={<i className={className} />}
         preProcessor={code => code.replace(/fill=".*?"/g, `fill="${color}"`).replace(/width=".*?"/g, '').replace(/height=".*?"/g, '')}
     />
 }
 
 export const useSVGOriginal = (svgName: string, className: string = 'w-4 h-4') => {
-    const defaultIcon: DefaultIconKeys = 'generic'
     return <SVG
         className={className}
         src={`/svg/${svgName}`}
         cacheRequests={true}
-        loader={defaultIcons[defaultIcon] || null}
+        loader={<i className={className} />}
         preProcessor={code => code.replace(/width=".*?"/g, '').replace(/height=".*?"/g, '')}
     />
 }
