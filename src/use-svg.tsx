1799648ef6a7128146d570ff503fa983bd66c9e6
Yasser Fadl <yasser.fadl@gmail.com>
2022-02-20 12:31:47 +0000

    fix: workaround the bug in reactinlinesvg

--- src/use-svg.tsx
+++ src/use-svg.tsx
@@ -1,22 +1,57 @@
-import React from 'react'
+
+   
+import React, { useMemo, useRef, useState } from 'react'
 import SVG from 'react-inlinesvg'
 
+type DefaultIconKeys = 'info' | 'user' | 'generic'
+const defaultIcons: Record<DefaultIconKeys, React.ReactElement | null> = {
+    info:  null,
+    user: null,
+    generic: null
+}
+
+const SVGWrapper = (props: any) => {
+    const svg = useRef<any>(null)
+    const [loaded, setLoaded] = useState(false)
+    const [lastSvg, setLastSvg] = useState(props.src)
+    
+    return useMemo(() => {
+        if (loaded) {
+            if (lastSvg !== props.src) {
+                setLoaded(false)
+                setLastSvg(props.src)
+                svg.current = <SVG {...props} onLoad={() => setLoaded(true)} />
+            }
+        }
+
+        if (!loaded) {
+            if (lastSvg === props.src) {
+                svg.current =  <SVG {...props} onLoad={() => setLoaded(true)} />
+            }
+        }
+
+        return svg.current
+    }, [loaded, props.src])
+}
+
 export const useSVG = (svgName: string, className: string = 'w-4 h-4', color: string = 'currentColor') => {
-    return <SVG
+    const defaultIcon: DefaultIconKeys = 'generic'
+    return <SVGWrapper
         className={className}
         src={`/svg/${svgName}`}
         cacheRequests={true}
-        loader={<i className={className} />}
-        preProcessor={code => code.replace(/fill=".*?"/g, `fill="${color}"`).replace(/width=".*?"/g, '').replace(/height=".*?"/g, '')}
+        loader={defaultIcons[defaultIcon] || null}
+        preProcessor={(code: string) => code.replace(/fill=".*?"/g, `fill="${color}"`).replace(/width=".*?"/g, '').replace(/height=".*?"/g, '')}
     />
 }
 
 export const useSVGOriginal = (svgName: string, className: string = 'w-4 h-4') => {
-    return <SVG
+    const defaultIcon: DefaultIconKeys = 'generic'
+    return <SVGWrapper
         className={className}
         src={`/svg/${svgName}`}
         cacheRequests={true}
-        loader={<i className={className} />}
-        preProcessor={code => code.replace(/width=".*?"/g, '').replace(/height=".*?"/g, '')}
+        loader={defaultIcons[defaultIcon] || null}
+        preProcessor={(code: string) => code.replace(/width=".*?"/g, '').replace(/height=".*?"/g, '')}
     />
 }
